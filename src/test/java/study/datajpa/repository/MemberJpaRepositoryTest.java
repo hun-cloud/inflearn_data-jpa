e01ba80751ccbbcfea51efe09cdd7a7905c41f6a
leejaehun <ljh0901@mlsoft.com>
2023-11-30 12:25:59 +0900

    메소드 이름으로 쿼리 생성

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -6,15 +6,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class MemberJpaRepositoryTest {
 
     @Autowired
     private MemberJpaRepository memberJpaRepository;
 
-    @Transactional
+
     @Test
     public void testMember() {
         Member member = new Member("memberA");
@@ -24,6 +27,45 @@ class MemberJpaRepositoryTest {
         Member findMember = memberJpaRepository.find(member.getId());
 
         assertThat(member).isEqualTo(findMember);
+    }
+
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("memberA", 15);
+        Member member2 = new Member("memberB", 15);
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        // 단건 조회 검
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThen() {
+        Member member1 = new Member("memberA", 15);
+        Member member2 = new Member("memberB", 15);
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        List<Member> memberA = memberJpaRepository.findByUsernameAndAgeGreaterThen("memberA", 13);
+        List<Member> memberB = memberJpaRepository.findByUsernameAndAgeGreaterThen("memberA", 16);
+
+        assertThat(memberA.size()).isEqualTo(1);
+        assertThat(memberB.size()).isEqualTo(0);
     }
 
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,5 +32,40 @@ class MemberRepositoryTest {
         assertThat(saveMember).isEqualTo(findMember);
     }
 
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("memberA");
+        Member member2 = new Member("memberB");
 
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        // 단건 조회 검
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThen() {
+        Member member1 = new Member("memberA", 15);
+        Member member2 = new Member("memberB", 15);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> memberA = memberRepository.findByUsernameAndAgeGreaterThan("memberA", 13);
+        List<Member> memberB = memberRepository.findByUsernameAndAgeGreaterThan("memberA", 16);
+
+        assertThat(memberA.size()).isEqualTo(1);
+        assertThat(memberB.size()).isEqualTo(0);
+    }
 }
